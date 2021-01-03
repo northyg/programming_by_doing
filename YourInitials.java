99c966691fa8d45ddce80c00de24bcb092f6422c
gnorthy <gnorthy@newrelic.com>
2021-01-02 23:39:13 -0800

    Update notes on YourInitialis.java

--- YourInitials.java
+++ YourInitials.java
@@ -1,6 +1,8 @@
 // https://programmingbydoing.com/a/your-initials.html
+// Link to tool that makes ASCII art letters: https://onlineasciitools.com/convert-text-to-ascii-art
 // Print your initials to stdout
 // I put the initials in a txt file, then read out the file
+
 
 import java.io.*;
 import java.util.*;
