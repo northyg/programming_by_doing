94c45b5782ab7413058fa02129c49ae3091cb25b
Giselle Northy <giselle.pwnz@gmail.com>
2021-02-06 21:56:08 -0800

    Create BoringWindow.java

---
+++ BoringWindow.java
@@ -0,0 +1,16 @@
+/*
+http://programmingbydoing.com/a/boring-window.html
+*/
+
+import java.util.*;
+import javax.swing.*;
+
+public class BoringWindow extends JFrame {
+
+    public static void main(String[] args) {
+        JFrame f = new BoringWindow();
+        f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        f.setSize(300, 200);
+        f.setVisible(true);
+    }
+}
