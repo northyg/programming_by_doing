2484cd6845fa61836df6b4540d663680e2dde3f9
Giselle Northy <giselle.pwnz@gmail.com>
2021-03-06 22:11:57 -0800

    Create CompareToChallenge.java

---
+++ CompareToChallenge.java
@@ -0,0 +1,17 @@
+/*
+https://programmingbydoing.com/a/compareto-challenge.html
+
+compareTo() Challenge
+Write a program that compares several Strings using the compareTo() method. You should display the Strings and display the integer that compareTo() gives you.
+
+You must have five examples which result in a number less than 0, five examples which result in a number greater than 0, and two examples which give you exactly 0. 
+This means you need a total of twelve examples.
+
+You may not just flip the Strings around; you must have twelve different examples.
+
+The Java String compareTo() method is used for comparing two strings lexicographically. 
+Each character of both the strings is converted into a Unicode value for comparison. 
+If both the strings are equal then this method returns 0 else it returns positive or negative value.
+The result is positive if the first string is lexicographically greater than the second string else the result would be negative.
+https://beginnersbook.com/2013/12/java-string-compareto-method-example/
+*/
