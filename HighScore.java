ee2058afdadd724cb8b5f6a746a7c0bfdf0e5a42
northyg <giselle.pwnz@gmail.com>
2021-03-25 12:59:17 -0700

    Works but with a couple bugs.

--- HighScore.java
+++ HighScore.java
@@ -1,6 +1,10 @@
 /*
+https://programmingbydoing.com/a/high-score.html
+
 Write a dumb little program that asks the user for their name and a number.
 Then store that name and that "high score" in a file. The file should be called "score.txt".
+
+Bugz: If the Int or String is anything but that, then the input ends up on the next line or cut off...
  */
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,25 +16,42 @@ public class HighScore {
         try{
             java.io.File myFile = new java.io.File("score.txt");
             FileWriter writer = new FileWriter(myFile);
-            writer.write(score);
-            writer.write(name);
+            writer.write(String.valueOf(score) + "\n");
+            writer.write(name + "\n");
             writer.close();
         } catch (IOException io){
             io.printStackTrace();
         }
+        System.out.println("Value's stored are: The score is " + score + "\tName is " + name);
         System.out.println("Data store into score.txt");
+    }
+
+    public static int getInt(int score, Scanner myScanner) {
+        // If number entered is not an int, then default is 0
+        System.out.print("Please enter your score: ");
+        if (myScanner.hasNextInt()) {
+            score = myScanner.nextInt();
+        }
+        return score;
+    }
+
+    public static String getString(String name, Scanner myScanner) {
+        System.out.print("Please enter your name: ");
+        if (myScanner.hasNext()) {
+            name = myScanner.next();
+            myScanner.close();
+        }
+        return name;
     }
 
     public static void main(String[] args) {
 
+        int score = 0;
+        String name = "SomeName";
         Scanner myScanner = new Scanner(System.in);
 
-        System.out.println("Please enter your score: ");
-        int score = myScanner.nextInt();
-
-        System.out.println("Please enter your name: ");
-        String name = myScanner.nextLine();
-
+        score = getInt(score, myScanner);
+        name = getString(name, myScanner);
         writeToFile(score, name);
     }
 }
